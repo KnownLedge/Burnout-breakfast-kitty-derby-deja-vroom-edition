415a657cac9aba499dd95c7d7e3dd14ae41f18f2
KnownLedge <knowlegekeeper360@gmail.com>
2025-08-03 12:55:47 +0100

    Results screen animations
    animation and effects done for results screen, now needs linked up to checkpoint script

---
+++ Breakfast Burnout/Assets/Scripts/Ui/FruitLoopsFill.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FruitLoopsFill : MonoBehaviour
+{
+    private Image image;
+    public bool fillOnAwake = false;
+    public bool shouldFill = false;
+
+    private float fillTimer = 1f;
+
+    public bool isFill = false;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        if (fillOnAwake)
+        {
+          shouldFill = true;  
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldFill)
+        {
+            image.fillAmount += fillTimer * Time.deltaTime;
+            if(image.fillAmount >= 1)
+                isFill = true;
+        }
+        else
+        {
+            image.fillAmount -= fillTimer * Time.deltaTime;
+            if (image.fillAmount <= 0)
+                isFill = false;
+        }
+    }
+}
---
+++ Breakfast Burnout/Assets/Scripts/Ui/ResultsScreen.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultsScreen : MonoBehaviour
+{
+    public Transform standingsObj;
+    public float lerpSpeed = 3f;
+    private int state = 0; //It is too late to do an enum (it would take 3 seconds)
+    public FruitLoopsFill FLScript;
+    public Vector3 centerPos;
+    private Vector3 startPos;
+    void Start()
+    {
+     startPos = standingsObj.localPosition;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FLScript.isFill)
+        {
+            if (state == 0)
+            {
+                standingsObj.localPosition = Vector3.Lerp(standingsObj.localPosition, centerPos, lerpSpeed * Time.deltaTime);
+
+                if (Vector3.Distance(standingsObj.localPosition, centerPos) < 9 && Input.GetButtonDown("Jump")) { 
+                state = 1;
+                }
+            }
+            else if (state == 1) { 
+                standingsObj.localPosition = Vector3.Lerp(standingsObj.localPosition, startPos, lerpSpeed * Time.deltaTime);
+
+                if (Vector3.Distance(standingsObj.localPosition, startPos) < 9)
+                {
+                    FLScript.shouldFill = false;
+                    state = 2;
+                }
+            }
+
+
+
+        }
+        else if(state == 2)
+        {
+            //LOAD NEXT SCENE HERE
+
+            gameObject.SetActive(false);
+        }
+    }
+}
